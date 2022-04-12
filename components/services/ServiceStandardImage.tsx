09b044533c3dfb378b935d0951a30add9162e0f7
Michel Busse <michel.busse@m-to-b.com>
2022-04-12 23:55:33 +0200

    Added Fade In to ServiceStandardImage

--- components/services/ServiceStandardImage.tsx
+++ components/services/ServiceStandardImage.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function ServiceStandardImage(props) {
   return (
     <>
-      <div className={styles.standardImage}>
+      <div className={styles.standardImage + " " + (props.active ? styles.active : "")}>
         <Image src={props.image} alt={props.imageAlt} layout="fill"/>
       </div>
     </>
