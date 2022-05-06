26ae0d98dcca04879ca162b88ddcb248e93b1ed9
Michel Busse <michel.busse@m-to-b.com>
2022-05-07 01:22:32 +0200

    Added <strong> Tags

--- pages/Softwareentwicklung.tsx
+++ pages/Softwareentwicklung.tsx
@@ -16,7 +16,7 @@ const DevelopmentPage: NextPage = () => {
         <title>Individuelle Softwareentwicklung aus Leipzig - M-to-B</title>
         <meta
           name="description"
-          content="Wir entwickeln Software mit Kreativität und Erfahrung. Profitieren Sie von modernen Technologien, kompetenter Zusammenarbeit und neuen Lösungen!"
+          content="Wir entwickeln Software mit Kreativität und Begeisterung. Profitieren Sie von modernen Technologien, kompetenter Zusammenarbeit und neuen Lösungen!"
         />
       </Head>
       <SoftwareHeader />
@@ -26,15 +26,16 @@ const DevelopmentPage: NextPage = () => {
             headline="Individualsoftware"
             text={[
               <p style={{ transitionDelay: "0.3s" }} key={1}>
-                Individuell entwickelte Software lohnt sich überall, wo
-                bestehende Programme die Anforderungen und Firmenprozesse nicht
-                optimal abbilden können, oder wo völlig neue Ideen umgesetzt
-                werden sollen.
+                <strong>Individuell entwickelte Software</strong> lohnt sich
+                überall, wo bestehende Programme die Anforderungen im
+                Unternehmen nicht optimal abbilden können, oder wo völlig neue
+                Ideen umgesetzt werden sollen.
               </p>,
               <p style={{ transitionDelay: "0.6s" }} key={2}>
-                Gemeinsam mit Ihnen erstellen wir maßgeschneiderte Lösungen,
-                welche alle Ziele treffen und Ihre Vorstellungen modern und
-                effizienz umsetzen.
+                Gemeinsam mit Ihnen erstellen wir{" "}
+                <strong>maßgeschneiderte Softwarelösungen</strong>, welche alle
+                Ziele treffen und Ihre Vorstellungen modern und effizienz
+                umsetzen.
               </p>,
               <p style={{ transitionDelay: "0.9s" }} key={3}>
                 Von Mitarbeitermanagement, Projektmanagement, Inventarsystemen
@@ -51,11 +52,11 @@ const DevelopmentPage: NextPage = () => {
               <p style={{ transitionDelay: "0.3s" }} key={1}>
                 Von der Idee bis zum Upload in den App Store führen wir Ihre App
                 durch den Entwicklungsprozess und zum Erfolg. Mit Erfahrung in
-                der Cross-Platform und nativen Entwicklung für Android und IOS
-                können wir Ihre Anforderungen effizient und modern
-                implementieren. Dabei legen wir auf verlässliche Funktionen
-                genauso viel Wert wie auf ein überzeugendes und nutzbares
-                UI-Design.
+                der <strong>Cross-Platform</strong> und nativen
+                <strong>App-Entwicklung</strong> für Android und IOS können wir
+                Ihre Anforderungen effizient und modern implementieren. Dabei
+                legen wir genauso viel Wert auf verlässliche Funktionen wie auf
+                ein überzeugendes und nutzbares UI-Design.
               </p>,
             ]}
             image="/images/Softwareentwicklung/smartphone.png"
@@ -69,16 +70,16 @@ const DevelopmentPage: NextPage = () => {
           }}
         >
           <ServiceSection
-            headline="IT Beratung"
+            headline="IT-Consulting"
             text={[
               <p style={{ transitionDelay: "0.3s" }} key={1}>
-                Wir beraten Sie ausführlich zu allen Möglichkeiten für Ihr
-                Projekt. Wir vergleichen für Sie verschiedene
-                Lösungsmöglichkeiten und finden gemeinsam mit Ihnen die passende
-                Technologie und Softwarearchitektur für Ihre Anwendung. Als
-                modernes IT-Unternehmen ist es unser Ziel, die beste Strategie
-                für Sie zu entwickeln und Zeit, Budget und Funktionalität
-                optimal abzustimmen.
+                Wir bieten ausführliche <strong>IT-Beratung</strong> zu allen
+                Möglichkeiten für Ihr Projekt. Dabei vergleichen wir für Sie
+                verschiedene Lösungsmöglichkeiten und finden gemeinsam mit Ihnen
+                die passende Technologie und Softwarearchitektur für Ihre
+                Anwendung. Als modernes <strong>IT-Unternehmen</strong> ist es
+                unser Ziel, die beste Strategie für Sie zu entwickeln und Zeit,
+                Budget und Funktionalität optimal abzustimmen.
               </p>,
             ]}
             image="/images/Softwareentwicklung/consulting.svg"
