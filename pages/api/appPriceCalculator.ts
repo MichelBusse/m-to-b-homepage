b6672445a10f62d099c817ca566c3e2c58824909
Michel Busse <michel.busse@m-to-b.com>
2023-01-10 17:56:31 +0100

    Changed Mailtext

--- pages/api/appPriceCalculator.ts
+++ pages/api/appPriceCalculator.ts
@@ -33,14 +33,22 @@ export default async function handler(
       "Mit freundlichen Grüßen,\n" +
       "Ihr Team von M-to-B\n\n\n" +
       "Entwicklungs-Aufwand Leitfaden:\n\n" +
-      "1. Sammeln Sie Informationen über die Anforderungen und Funktionen der App: Bevor Sie den Preis für die App-Entwicklung berechnen können, benötigen Sie ein klares Verständnis davon, was die App tun soll und welche Funktionen sie haben wird. Stellen Sie Fragen, um diese Anforderungen zu erfassen, und sammeln Sie so viele Details wie möglich.\n" +
-      "2. Berücksichtigen Sie die Plattformen, auf denen die App entwickelt werden soll: Wird die App für iOS, Android oder beide Plattformen entwickelt werden? Wenn sie für mehrere Plattformen entwickelt werden soll, wird dies den Preis erhöhen.\n" +
-      "3. Berücksichtigen Sie das Design der App: Wird das Design von Grund auf neu entwickelt werden müssen, oder gibt es bereits vorhandene Designs, die verwendet werden können? Ein umfangreiches, benutzerdefiniertes Design wird den Preis erhöhen.\n" +
-      "4. Berücksichtigen Sie die Komplexität der Funktionen: Je mehr Funktionen die App hat, desto teurer wird ihre Entwicklung sein. Berücksichtigen Sie auch, ob die App integriert werden muss mit bestehenden Systemen oder Datenbanken, da dies den Preis ebenfalls erhöhen wird.\n" +
-      "5. Berücksichtigen Sie Benutzerauthentifizierung und Sicherheitsfunktionen: Wird die App Benutzerauthentifizierung oder Sicherheitsfunktionen wie Verschlüsselung benötigen? Diese Funktionen können den Preis erhöhen.\n" +
-      "6. Berücksichtigen Sie E-Commerce-Funktionalitäten: Wird die App E-Commerce-Funktionalitäten enthalten, wie das Hinzufügen von Produkten zum Warenkorb oder das Durchführen von Zahlungen? Dies wird den Preis erhöhen.\n" +
-      "7. Berücksichtigen Sie regelmäßige Updates und Wartung: Wird es regelmäßige Updates oder Wartung der App geben, die in den Preis einbezogen werden müssen?\n" +
-      "8. Berücksichtigen Sie die Leistungsanforderungen: Gibt es besondere Anforderungen an die Leistung der App, wie zum Beispiel hohe Verarbeitungsgeschwindigkeit? Dies kann den Preis erhöhen.\n",
+      "1. Sammeln Sie Informationen über die Anforderungen und Funktionen der App:\n" +
+      "Bevor Sie den Preis für die App-Entwicklung berechnen können, benötigen Sie ein klares Verständnis davon, was die App tun soll und welche Funktionen sie haben wird. Stellen Sie Fragen, um diese Anforderungen zu erfassen, und sammeln Sie so viele Details wie möglich.\n\n" +
+      "2. Berücksichtigen Sie die Plattformen, auf denen die App entwickelt werden soll:\n" +
+      "Wird die App für iOS, Android oder beide Plattformen entwickelt werden? Wenn sie für mehrere Plattformen entwickelt werden soll, wird dies den Preis erhöhen.\n\n" +
+      "3. Berücksichtigen Sie das Design der App:\n" +
+      "Wird das Design von Grund auf neu entwickelt werden müssen, oder gibt es bereits vorhandene Designs, die verwendet werden können? Ein umfangreiches, benutzerdefiniertes Design wird den Preis erhöhen.\n\n" +
+      "4. Berücksichtigen Sie die Komplexität der Funktionen:\n" +
+      "Je mehr Funktionen die App hat, desto teurer wird ihre Entwicklung sein. Berücksichtigen Sie auch, ob die App integriert werden muss mit bestehenden Systemen oder Datenbanken, da dies den Preis ebenfalls erhöhen wird.\n\n" +
+      "5. Berücksichtigen Sie Benutzerauthentifizierung und Sicherheitsfunktionen:\n" +
+      "Wird die App Benutzerauthentifizierung oder Sicherheitsfunktionen wie Verschlüsselung benötigen? Diese Funktionen können den Preis erhöhen.\n\n" +
+      "6. Berücksichtigen Sie E-Commerce-Funktionalitäten:\n" +
+      "Wird die App E-Commerce-Funktionalitäten enthalten, wie das Hinzufügen von Produkten zum Warenkorb oder das Durchführen von Zahlungen? Dies wird den Preis erhöhen.\n\n" +
+      "7. Berücksichtigen Sie regelmäßige Updates und Wartung:\n" +
+      "Wird es regelmäßige Updates oder Wartung der App geben, die in den Preis einbezogen werden müssen?\n\n" +
+      "8. Berücksichtigen Sie die Leistungsanforderungen:\n" +
+      "Gibt es besondere Anforderungen an die Leistung der App, wie zum Beispiel hohe Verarbeitungsgeschwindigkeit? Dies kann den Preis erhöhen.\n\n",
   };
 
   let mailRes = await new Promise((resolve, reject) => {
